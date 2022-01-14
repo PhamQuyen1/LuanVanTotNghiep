67231ceac85532feaa3f71f114fabdd8baf70633
Pham Quyen <pquyen180@gmail.com>
2022-01-14 20:31:57 +0700

    project initialization

---
+++ src/main/java/com/phamquyen/luanvan/LuanVanApplication.java
@@ -0,0 +1,13 @@
+package com.phamquyen.luanvan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LuanVanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LuanVanApplication.class, args);
+    }
+
+}
